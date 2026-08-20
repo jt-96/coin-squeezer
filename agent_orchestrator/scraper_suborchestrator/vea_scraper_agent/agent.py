ebe26d890209e99517a2f1fcd5a9ccc65b69f573
jt-96 <jetorrico@outlook.com>
2026-08-20 15:21:04 -0300

    feat: add mcp imports, swap instruction for testing, add mcp to tools

--- agent_orchestrator/scraper_suborchestrator/vea_scraper_agent/agent.py
+++ agent_orchestrator/scraper_suborchestrator/vea_scraper_agent/agent.py
@@ -1,13 +1,31 @@
 from google.adk.agents.llm_agent import Agent
+from google.adk.tools.mcp_tool import McpToolset
+from google.adk.tools.mcp_tool.mcp_session_manager import StdioConnectionParams
+from mcp import StdioServerParameters
 
 vea_scraper_agent = Agent(
     model='gemini-3.5-flash',
     name='vea_scraper_agent',
     description='A web scraper that extracts values from the Vea Supermarket site',
-    instruction=""" You are a web scraper and data extrator specialist.
+    # instruction=""" You are a web scraper and data extrator specialist.
 
-    Your job is to scrap the contents of websites, and obtain the name and price of each of the items provided in that list of sites.
+    # Your job is to scrap the contents of websites, and obtain the name and price of each of the items provided in that list of sites using the tools provided.
 
     
+    # """,
+    instruction=""" You are a web scraper and data extractor specialist
+
+    Your job is to scrap the contents of websites, and obtain the name and price of a link provided to you, using the tools available.
+    
     """,
+    tools=[
+        McpToolset(
+            connection_params=StdioConnectionParams(
+                server_params=StdioServerParameters(
+                    command="npx",
+                    args=["-y", "chrome-devtools-mcp@latest", "--headless"]
+                )
+            )
+        )
+    ]
 )
