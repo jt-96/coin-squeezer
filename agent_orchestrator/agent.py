8fb969749f17587e8709f0ee83d1c7f49ba91871
jt-96 <jetorrico@outlook.com>
2026-08-24 17:39:13 -0300

    feat: include data_parser as subagent

--- agent_orchestrator/agent.py
+++ agent_orchestrator/agent.py
@@ -1,6 +1,7 @@
 # from google.adk.agents.llm_agent import Agent
 from google.adk.agents.sequential_agent import SequentialAgent
 from .scraper_suborchestrator.agent import scraper_suborchestrator_agent
+from .data_parser_agent.agent import data_parser_agent
 
 root_agent = SequentialAgent(
     # model='gemini-3.5-flash',
@@ -11,5 +12,5 @@ root_agent = SequentialAgent(
     # Your job is to make sure that the sub-orchestrator and sub-agents you manage complete their work in the specified order.
     # """,
     name="pipeline",
-    sub_agents=[scraper_suborchestrator_agent]
+    sub_agents=[scraper_suborchestrator_agent, data_parser_agent]
 )
