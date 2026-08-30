23e399a75bfa07e27a47faf991b860f7b8dc0ba1
jt-96 <jetorrico@outlook.com>
2026-08-30 17:42:45 -0300

    feat: add missing imports and improve descriptions

--- agent_orchestrator/agent.py
+++ agent_orchestrator/agent.py
@@ -1,16 +1,11 @@
-# from google.adk.agents.llm_agent import Agent
 from google.adk.agents.sequential_agent import SequentialAgent
 from .scraper_suborchestrator.agent import scraper_suborchestrator_agent
 from .data_parser_agent.agent import data_parser_agent
+from .database_agent.agent import database_agent
+from .email_agent.agent import email_agent
 
 root_agent = SequentialAgent(
-    # model='gemini-3.5-flash',
-    # name='orchestrator_agent',
-    # description='Relays work to subagents and obtains their result',
-    # instruction=""" You are an orchestrator specialist
-    
-    # Your job is to make sure that the sub-orchestrator and sub-agents you manage complete their work in the specified order.
-    # """,
+    description='Relays work to subagents and pass their result to the next agent.',
     name="pipeline",
-    sub_agents=[scraper_suborchestrator_agent, data_parser_agent]
+    sub_agents=[scraper_suborchestrator_agent, data_parser_agent, database_agent, email_agent]
 )
--- agent_orchestrator/scraper_suborchestrator/agent.py
+++ agent_orchestrator/scraper_suborchestrator/agent.py
@@ -1,15 +1,11 @@
 # from google.adk.agents.llm_agent import Agent
 from google.adk.agents.parallel_agent import ParallelAgent
 from .vea_scraper_agent.agent import vea_scraper_agent
+from .mas_scraper_agent.agent import mas_scraper_agent
+from .carrefour_scraper_agent.agent import carrefour_scraper_agent
 
 scraper_suborchestrator_agent = ParallelAgent(
-    # model='gemini-3.5-flash',
-    # name='scraper_suborchestrator',
-    # description='A orchestrator that manages the scrapers work and data',
-    # instruction=""" You are an orchestrator of subagents
-    
-    # Your main job is to manage and control the work of your scrapers subagents, reporting when their job is complete to relay the data obtained by the scrapers to the next step of the specified order of execution.
-    # """,
+    description='A suborchestrator that manages the subagents in change of data extraction.',
     name="parallel_scrapers",
-    sub_agents=[vea_scraper_agent]
+    sub_agents=[vea_scraper_agent, mas_scraper_agent, carrefour_scraper_agent]
 )
