9e492d2a936d8fff96e067b75bbb10fb281f0bfb
jt-96 <jetorrico@outlook.com>
2026-08-30 17:43:13 -0300

    fix: add missing description

--- agent_orchestrator/agent.py
+++ agent_orchestrator/agent.py
@@ -5,7 +5,7 @@ from .database_agent.agent import database_agent
 from .email_agent.agent import email_agent
 
 root_agent = SequentialAgent(
-    description='Relays work to subagents and pass their result to the next agent.',
+    description='An orchestrator that relays work to subagents and pass their result to the next agent.',
     name="pipeline",
     sub_agents=[scraper_suborchestrator_agent, data_parser_agent, database_agent, email_agent]
 )
