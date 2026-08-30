094048c70dbb22d43e88c1d51c1b750e1b93eb77
jt-96 <jetorrico@outlook.com>
2026-08-30 17:37:06 -0300

    feat: swap to workspace-mcp, improve specs, remove auth generator file

--- agent_orchestrator/email_agent/agent.py
+++ agent_orchestrator/email_agent/agent.py
@@ -1,10 +1,10 @@
-from google.adk.tools.mcp_tool.mcp_session_manager import SseServerParams
+import os
 import asyncio
 from google.adk.agents.llm_agent import Agent
-from google.adk.tools.mcp_tool import StdioConnectionParams
 from google.adk.tools.mcp_tool import McpToolset
+from google.adk.tools.mcp_tool import StdioConnectionParams
+from mcp import StdioServerParameters
 from ratelimit import limits, sleep_and_retry
-from auth import get_workspace_access_token 
 
 @sleep_and_retry
 @limits(calls=4, period=60)
@@ -24,45 +24,40 @@ async def rate_limit_model_callback(callback_context, llm_request):
     await asyncio.to_thread(safe_model_limit_trigger)
     return None
 
-active_token = get_workspace_access_token()
+GOOGLE_USER_EMAIL_SENDER = os.environ.get("GOOGLE_USER_EMAIL_SENDER")
+GOOGLE_USER_EMAIL_DESTINATION = os.environ.get("GOOGLE_USER_EMAIL_DESTINATION")
 
 email_agent = Agent(
     model='gemini-3.5-flash',
     name='email_agent',
     description='An assistant in charge of email parsing and communications.',
-    # instruction=""" You are an email assistant.
-    
-    # Your role is to parse the data recieved from the previous agent in the order of execution and send it through email.
+    instruction=f"""You are an email assistant
 
-    # This data {parsed_data} should be formatted as a table, containing the name of the product and each price from the stores.
+    Your role is to parse the data recieved from the previous agent in the order of execution and send it through email, using the connected MCP tool.
 
-    # Once formatted, you can add a small message before hand in the body of the email, saying "Offers updated, check here or on the site!".
+    This data {{parsed_data}} should be formatted as a table, containing the name of the product and each price from the stores.
 
-    # After that comes the formatted table, this email should be send using the tools provided, to the following email "jetorrico@gmail.com", with the subject "Coin-Squeezer Notification!".
-
-    # Once finished, respond with "Email has been sent!" to finish, if there's an error during any step of this instruction, do notify.
-    # """,
-    instruction="""You are an email assistant
-
-    Your role is to use the connected Gmail MCP server tools to send an email.
-    Recipient: "jetorrico@gmail.com"
-    Subject: "Test Email from Agent"
-    Body: "Hi. This is an email from an Agent"
+    From: {GOOGLE_USER_EMAIL_SENDER}
+    Recipient: {GOOGLE_USER_EMAIL_DESTINATION}
+    Subject: "Coin Squeezer Notification!"
+    Body: "There are new offers to check! *PARSED TABLE GOES HERE*"
     
     If there's an error during the operation, say so, otherwise respond with "Email Sent!" to finish.
     """,
     tools=[
         McpToolset(
             connection_params=StdioConnectionParams(
-                server_params=SseServerParams(
-                    uri="https://gmailmcp.googleapis.com/mcp/v1",
-                    args= ["-y", "@google/workspace-mcp-server", "--credentials", "./secrets/email_oauth_credentials.json", "--stdio"],
-                    headers={
-                    "Authorization": f"Bearer {active_token}"
+                server_params=StdioServerParameters(
+                    command="uvx",
+                    args=["workspace-mcp", "--tools", "gmail"],
+                    env={
+                        "GOOGLE_OAUTH_CLIENT_ID": os.environ.get("GOOGLE_OAUTH_CLIENT_ID"),
+                        "GOOGLE_OAUTH_CLIENT_SECRET": os.environ.get("GOOGLE_OAUTH_CLIENT_SECRET")
                     }
                 ),
-                timeout=20
-            )
+                timeout=30,
+            ),
+            tool_filter=["send_gmail_message"],
         )
     ],
     before_tool_callback=rate_limit_tool_callback,
